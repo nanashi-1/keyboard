6618bddfc7696d8d2b0a2933aa518a02cfb84341
nanashi <bagaresalken@gmail.com>
2022-04-02 10:10:46 +0800

    tweaked Synth

--- js/stimulus.js
+++ js/stimulus.js
@@ -5,8 +5,21 @@ let synths = {}
 document.addEventListener('keydown', function (event) {
     event.preventDefault()
     if (!_keys[event.key]) {
-        synths[event.key] = new Tone.Synth().toDestination();
-        synths[event.key].triggerAttack(notes[event.key], "8n")
+        synths[event.key] = new Tone.Synth({
+            "oscillator" : {
+                "type" : "sine",
+                "modulationFrequency" : 1
+            },
+            "envelope" : {
+                "attack" : 0.02,
+                "decay" : 0.1,
+                "sustain" : 0.2,
+                "release" : 2,
+            }
+        }).toDestination();
+        const now = Tone.now()
+        synths[event.key].triggerAttack(notes[event.key], now)
+        synths[event.key].triggerRelease(now + 1)
         let button = document.getElementById(event.key)
         button.style.backgroundColor = "rgb(120, 120, 120)"
         _keys[event.key] = true
